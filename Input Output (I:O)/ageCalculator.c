da65cd8167b3093988d8b480f74daa97f60daa51
Himanshu Joshi <himanshujoshi790@gmail.com>
2019-09-10 19:23:36 -0500

    more on I/O

---
+++ Input Output (I:O)/ageCalculator.c
@@ -0,0 +1,12 @@
+/*
+This small script asks you to enter your birthday and gives you your current age
+and the age in days and how many days until your next birthday. 
+*/
+
+
+#include <stdio.h>
+
+int main(void)
+{
+  printf("%s\n", );
+}
---
+++ Input Output (I:O)/basicio.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    //variables
+    int Integer = 5;
+    float Float = 13.5;
+    double Double = 12.4;
+    char chr = 'a';
+
+    //print statements
+    printf("An integer = %d\n", Integer);
+    printf("A Float = %f\n", Float);
+    printf("A Double = %lf\n", Double);
+    printf("A character = %c.\n", chr);
+
+    return 0;
+}
