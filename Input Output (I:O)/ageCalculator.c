96efddf3f4d1941ab4b8ff315cbfacf3e44cb9f1
Himanshu Joshi <himanshujoshi790@gmail.com>
2019-09-10 20:06:58 -0500

    ageCalculator added

--- Input Output (I:O)/ageCalculator.c
+++ Input Output (I:O)/ageCalculator.c
@@ -1,6 +1,6 @@
 /*
 This small script asks you to enter your birthday and gives you your current age
-and the age in days and how many days until your next birthday. 
+and the age in days and how many days until your next birthday.
 */
 
 
@@ -8,5 +8,14 @@ and the age in days and how many days until your next birthday.
 
 int main(void)
 {
-  printf("%s\n", );
+  int birthday[8];
+  printf("Welcome to age calculator, follow the instructions!\n");
+  printf("What's your birthday? (MMDDYYYY)");
+  for (int i = 0; i < 7; i++){
+  scanf("%d", &birthday[i]);
+  }
+
+  printf("You entered : %d", *birthday);
+
+  return 0;
 }
