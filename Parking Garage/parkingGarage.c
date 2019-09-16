a6828dd10d9df699fdb1022969b5a237f99608f0
Himanshu Joshi <himanshujoshi790@gmail.com>
2019-09-16 17:38:14 -0500

    Update parkingGarage.c

--- Parking Garage/parkingGarage.c
+++ Parking Garage/parkingGarage.c
@@ -4,5 +4,17 @@
 
 
 int main() {
-  
+
+  while(1){
+    printf("======\n", );
+    printf("Welcome to parkingGarage for xyz, Select one of these options:\n");
+    printf("======\n", );
+    printf("1. Show all open parking spots.."\n);
+    printf("2. Check parking status for a spot number?"\n);
+    printf("3. "\n);
+    printf("4. "\n);
+
+
+  }
+
 }
