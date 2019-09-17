84d1c408b17b17ddf20f5af962b7284a6030c3e7
Himanshu Joshi <himanshujoshi790@gmail.com>
2019-09-16 21:32:14 -0500

    Functionality for menu

--- Parking Garage/parkingGarage.c
+++ Parking Garage/parkingGarage.c
@@ -4,15 +4,19 @@
 
 
 int main() {
+  char action;
+  char building;
+  int floorNumber;
+  int spotNumber;
 
   while(1){
     printf("==============================================================\n");
     printf("Welcome to parkingGarage for xyz, Select one of these options:\n");
     printf("==============================================================\n");
-    printf("1. Park/ Unpark your car."\n);
-    printf("2. Check parking status for a spot number?"\n);
-    printf("3. Show all open parking spots.."\n);
-    printf("4. Show all spots."\n);
+    printf("1. Park/ Unpark your car.\n");
+    printf("2. Show all open parking spots..\n");
+    printf("3. Show all spots.\n");
+    printf("4. Exit \n");
     printf("----------------------------------------------------------------");
     printf("Enter Selection:");
     char selection;
@@ -20,16 +24,57 @@ int main() {
 
     switch(selection) {
       case '1' :
+
+      while(1){
+        printf("Enter P to park and U to unpark a car\n", );
+        scanf("%c", &action);
+        if (action== "P"|| action == "U"){
+          break;
+        }
+        printf("wrong input, try again.\n")
+      }
+
+      while(1){
+      printf("Enter building (A,B or C):");
+      scanf("%c", &building);
+      if (building =="A" ||building =="B"||building =="C"){
+        break;
+      }
+      printf("wrong input, try again.\n")
+      }
+
+
+      while(1){
+      printf("Now enter the floor (1 to 6):");
+      scanf("%d", &floorNumber);
+      if (floorNumber>0 || floorNumber<7){
+        break;
+      }
+      printf("wrong input, try again.\n")
+      }
+
+      while(1){
+      printf("What's your parking spot(1-100)?");
+      scanf("%d", &spotNumber);
+      if (floorNumber>0 || floorNumber<101){
+        break;
+      }
+      printf("wrong input, try again.\n")
+      }
+
+      park_unpark(action, building, floorNumber -1, spotNumber-1);
       break;
 
       case '2' :
+      showOpenSpots();
       break;
 
       case '3' :
-      showOpenSpots();      
+      showAllSpots();
       break;
 
       case '4' :
+      exit();
       break;
 
       default  :
--- Parking Garage/parkingGarage.h
+++ Parking Garage/parkingGarage.h
@@ -13,11 +13,95 @@ for (int i = 0; i < 3; i++){                 // The three buildings: A, B and C
   }
 }
 
-int park_unpark(int )
+int park_unpark(char action, char building, int floorNumber,int spotNumber){
+  int building_number;
 
+  if (building == "A"){
+    building_number = 0;
+  }
+  else if (building == "B") {
+    building_number = 1;
+  }
+  else if (building == "C") {
+    building_number = 2;
+  }
 
-void showOpenSpots(){
+  if (action == "P"){
+    if (parkingData[building_number][floorNumber][spotNumber] != 0){
+      printf("Error while parking your car, the space is already reserved. Try again\n");
+    }
+    else{
+      parkingData[building_number][floorNumber][spotNumber] = 1;
+      printf("Car unparked!\n" );
+    }
+  }
+
+  if (action == "U"){
+    if (parkingData[building_number][floorNumber][spotNumber] != 1){
+      printf("Error while unparking your car, there is no car in that spot. Try again\n");
+    }
+    else{
+      parkingData[building_number][floorNumber][spotNumber] = 0;
+      printf("Car unparked!\n" );
+    }
+  }
 
 }
 
-void
+
+void showOpenSpots(){
+  for (int i = 0; i < 3; i++){
+      if (i == 0) {
+        printf("Building A: ");
+      }
+      else if (i ==1) {
+        printf("Building B: ");
+      }
+      else if (i ==2){
+        printf("Buliding C: ");
+      }
+    for (int j = 0; j < 6; j++){
+      printf("Floor: %d Open Spots: ", j+1);
+      for (int z = 0; z < 100; z++){
+        if (parkingGarage[i][j][z] == 0){
+          printf("%d, ", z+1);
+        }
+        if (z == 99){
+          printf("\n");
+        }
+
+      }
+    }
+  }
+
+}
+
+void showAllSpots(){
+  for (int i = 0; i < 3; i++){
+      if (i == 0) {
+        printf("Building A: ");
+      }
+      else if (i ==1) {
+        printf("Building B: ");
+      }
+      else if (i ==2){
+        printf("Buliding C: ");
+      }
+    for (int j = 0; j < 6; j++){
+      printf("Floor: %d All Spots: ", j+1);
+      for (int z = 0; z < 100; z++){
+        if (parkingGarage[i][j][z] == 0){
+          printf("[%d: Open]", z+1);
+        }
+        else{
+          printf("[%d: Reserved]");
+        }
+        if (z == 99){
+          printf("\n");
+        }
+
+      }
+    }
+  }
+
+}
